9587faeee9fdc568d8aa0d9410b6005b1efc1225
Remy Sharp <remy@remysharp.com>
2016-08-19 12:27:18 +0100

    chore: tidy code up, factor out common parts

--- cli/index.js
+++ cli/index.js
@@ -1,3 +1,16 @@
 #!/usr/bin/env node
 const clite = require('clite');
-clite(require('./config'));
+clite({
+  commands: {
+    client: 'lib/client',
+    server: 'lib/server',
+    _: 'cli/client',
+  },
+  options: ['env'],
+  alias: { e: 'env', d: 'dev', V: 'verbose' },
+  booleans: [
+    'verbose',
+    'dev',
+  ],
+  help: 'usage.txt',
+});
