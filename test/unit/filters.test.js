73851dca49601353d8e2b459e766f191e30cc5a7
Remy Sharp <remy@remysharp.com>
2016-08-24 12:12:52 +0100

    feat: support body level filtering

---
+++ test/unit/filters.test.js
@@ -0,0 +1,61 @@
+const tap = require('tap').test;
+const Filters = require('../../lib/filters');
+
+tap('filter on body', t => {
+  const filter = Filters(__dirname + '/../fixtures/relay.json');
+
+  t.plan(7);
+  t.ok('filters loaded');
+
+  filter({
+    url: '/',
+    method: 'POST',
+    body: {
+      commits: [
+        {
+          modified: ['package.json', 'file1.txt']
+        }
+      ]
+    }
+  }, (error, res) => {
+    t.equal(error, null, 'no error');
+    t.equal(res, '/', 'allows the path request');
+  });
+
+  filter({
+    url: '/',
+    method: 'POST',
+    body: {
+      commits: [
+        {
+          modified: ['file2.txt']
+        },
+        {
+          modified: ['.snyk', 'file1.txt']
+        }
+      ]
+    }
+  }, (error, res) => {
+    t.equal(error, null, 'no error');
+    t.equal(res, '/', 'allows the path request');
+  });
+
+  filter({
+    url: '/',
+    method: 'POST',
+    body: {
+      commits: [
+        {
+          modified: ['file2.txt']
+        },
+        {
+          modified: ['file3.txt', 'file1.txt']
+        }
+      ]
+    }
+  }, (error, res) => {
+    t.equal(error.message, 'blocked', 'has been blocked');
+    t.equal(res, undefined, 'no follow allowed');
+  });
+
+});
