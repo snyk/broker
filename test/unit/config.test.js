6249ede4c008ede916c8fb93ee3fca6cdeb7ac01
Gareth Visagie <gareth@gjvis.com>
2016-11-16 14:40:02 +0000

    fix: fix expansion of $FOO/BAR in config

--- test/unit/config.test.js
+++ test/unit/config.test.js
@@ -3,10 +3,12 @@ const test = require('tap-only');
 test('config', t => {
   const foo = process.env.FOO = 'bar';
   const token = process.env.BROKER_TOKEN = '1234';
+  process.env.FOO_BAR = '$FOO/bar';
 
   const config = require('../../lib/config');
 
   t.equal(config.foo, foo, 'foo');
   t.equal(config.brokerToken, token, 'BROKER_TOKEN');
+  t.equal(config.fooBar, 'bar/bar');
   t.end();
 });
