bbcf319a37b280594e554604b91d14e60267ea50
Aviad Reich <aviad@snyk.io>
2017-03-22 16:21:24 +0200

    feat: add clients' health endpoint - GET /broker/:id
    BREAKING CHANGE: GET /broker returns a client's connection status.

--- test/functional/healthcheck.test.js
+++ test/functional/healthcheck.test.js
@@ -12,6 +12,8 @@ test('proxy requests originating from behind the broker client', t => {
    * 1. start broker in server mode
    * 2. start broker in client mode and join (1)
    * 3. check /healthcheck on client and server
+   * 4. stop client and check it's on "disconnected" in the server
+   * 5. restart client with same token, make sure it's not in "disconnected"
    */
 
   process.env.ACCEPT = 'filters.json';
@@ -20,19 +22,23 @@ test('proxy requests originating from behind the broker client', t => {
   process.env.BROKER_TYPE = 'server';
   const serverPort = port();
   const server = app.main({ port: serverPort });
+  const BROKER_TOKEN = '12345';
 
   process.chdir(path.resolve(root, '../fixtures/client'));
   process.env.BROKER_TYPE = 'client';
-  process.env.BROKER_TOKEN = '12345';
+  process.env.BROKER_TOKEN = BROKER_TOKEN;
   process.env.BROKER_SERVER_URL = `http://localhost:${serverPort}`;
   const clientPort = port();
-  const client = app.main({ port: clientPort });
+  let client = app.main({ port: clientPort });
 
-  t.plan(3);
+  t.plan(6);
 
+  const serverHealth = `http://localhost:${serverPort}/healthcheck`;
+  const serverClientHealth = `http://localhost:${serverPort}/` +
+          `broker/${BROKER_TOKEN}`;
+  const clientHealth = `http://localhost:${clientPort}/healthcheck`;
   t.test('server healthcheck', t => {
-    const url = `http://localhost:${serverPort}/healthcheck`;
-    request({url, json: true }, (err, res) => {
+    request({url: serverHealth, json: true }, (err, res) => {
       if (err) { return t.threw(err); }
 
       t.equal(res.statusCode, 200, '200 statusCode');
@@ -45,14 +51,48 @@ test('proxy requests originating from behind the broker client', t => {
   server.io.once('connection', socket => {
     socket.once('identify', () => {
       t.test('client healthcheck', t => {
-        const url = `http://localhost:${clientPort}/healthcheck`;
-        request({url, json: true }, (err, res) => {
+        request({url: clientHealth, json: true }, (err, res) => {
           if (err) { return t.threw(err); }
 
           t.equal(res.statusCode, 200, '200 statusCode');
           t.equal(res.body['ok'], true, '{ ok: true } in body');
           t.end();
         });
+      });
+
+      t.test('serve client-health with connected client', t => {
+        request({url: serverClientHealth, json: true }, (err, res) => {
+          if (err) { return t.threw(err); }
+
+          t.equal(res.statusCode, 200, '200 statusCode');
+          t.equal(res.body['ok'], true, '{ ok: true } in body');
+          t.end();
+        });
+      });
+
+      t.test('server client-health after client disconnected', t => {
+        client.close();
+        setTimeout(() => {
+          request({url: serverClientHealth, json: true }, (err, res) => {
+            if (err) { return t.threw(err); }
+
+            t.equal(res.statusCode, 404, '404 statusCode');
+            t.end();
+          });
+        }, 100);
+      });
+
+      t.test('server client-health after client re-connected', t => {
+        client = app.main({ port: clientPort });
+        setTimeout(() => {
+          request({url: serverClientHealth, json: true }, (err, res) => {
+            if (err) { return t.threw(err); }
+
+            t.equal(res.statusCode, 200, '200 statusCode');
+            t.equal(res.body['ok'], true, '{ ok: true } in body');
+            t.end();
+          });
+        }, 20);
       });
 
       t.test('clean up', t => {
