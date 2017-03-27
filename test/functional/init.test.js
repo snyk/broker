fdaefa1ba9fa231e2a215d268043763aac9b2f6c
Gareth Visagie <gareth@gjvis.com>
2017-03-27 16:34:39 +0100

    test: add high-level `broker init` test

---
+++ test/functional/init.test.js
@@ -0,0 +1,27 @@
+const fs = require('then-fs');
+const test = require('tap-only');
+const tmp = require('tmp');
+
+const init = require('../../cli/init');
+
+tmp.setGracefulCleanup(); // always remove temporary directories
+
+test('init creates files from specified client-templates', t => {
+  const originalWorkDir = process.cwd();
+  t.teardown(() => process.chdir(originalWorkDir));
+
+  tmp.dir({ unsafeCleanup: true }, (err, path) => {
+    if (err) { throw err; }
+    process.chdir(path);
+
+    init({_: ['snyk']})
+    .then(() => Promise.all([
+      fs.stat('.env'),
+      fs.stat('accept.json'),
+    ]))
+    .then(stats => {
+      t.ok(stats.every(Boolean), 'all templated files created');
+      t.end();
+    });
+  });
+});
