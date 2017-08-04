ca6e72ef1ce0179bd27e517866c33680ee003156
Anton Drukh <anton@snyk.io>
2017-08-04 21:08:13 +0300

    feat: allow header values substition

--- test/unit/relay-response-headers.test.js
+++ test/unit/relay-response-headers.test.js
@@ -9,40 +9,43 @@ tap.beforeEach(done => {
   done();
 });
 
-test('relay swaps values found in BROKER_VAR_SUB', t => {
+test('relay swaps header values found in BROKER_VAR_SUB', t => {
   const brokerToken = 'test-broker';
 
   const config = {
-    HOST: 'localhost',
-    PORT: '8001',
+    SECRET_TOKEN: 'very-secret',
+    VALUE: 'some-special-value',
   };
 
   const relay = proxyquire('../../lib/relay', {
-    'request': (options, fn) => {
+    request: (options, fn) => {
       spy(options);
       fn(null, { statusCode: 200 }, true);
-    }
+    },
   }).response;
 
   const route = relay([{
     method: 'any',
-    url: '/*'
+    url: '/*',
   }], config)(brokerToken);
 
-  const body = {
-    BROKER_VAR_SUB: ['url'],
-    url: '${HOST}:${PORT}/webhook'
+  const headers = {
+    BROKER_VAR_SUB: 'private-token,replaceme',
+    donttouch: 'not to be changed ${VALUE}',
+    'private-token': 'Bearer ${SECRET_TOKEN}',
+    replaceme: 'replace ${VALUE}',
   };
 
   route({
     url: '/',
-    method: 'POST',
-    body: Buffer.from(JSON.stringify(body)),
-    headers: {},
+    method: 'GET',
+    headers: headers,
   }, () => {
     t.equal(spy.callCount, 1, 'request placed');
     const arg = spy.args[0][0];
-    t.equal(JSON.parse(arg.body).url, `${config.HOST}:${config.PORT}/webhook`);
+    t.equal(arg.headers['private-token'], `Bearer ${config.SECRET_TOKEN}`);
+    t.equal(arg.headers.replaceme, `replace ${config.VALUE}`);
+    t.equal(arg.headers.donttouch, 'not to be changed ${VALUE}');
     t.end();
   });
 
