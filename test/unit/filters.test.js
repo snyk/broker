3e5654e7df9c9bb547da69d6db4b303c825d4dee
Yulia Bar-On <yulia@snyk.io>
2020-03-15 18:31:14 +0200

    fix: discard any information in url after a fragment identifier

--- test/unit/filters.test.js
+++ test/unit/filters.test.js
@@ -5,6 +5,46 @@ const Filters = require('../../lib/filters');
 
 const jsonBuffer = (body) => Buffer.from(JSON.stringify(body));
 
+test('Filter on URL', t => {
+  t.test('for GitHub private filters', (t) => {
+    t.plan(3);
+    
+    const ruleSource = require(__dirname + '/../fixtures/accept/github.json');
+    const filter = Filters(ruleSource.private);
+    t.pass('Filters loaded');
+
+    t.test('should allow valid /repos path to manifest', (t) => {
+      const url = '/repos/angular/angular/contents/package.json';
+      
+      filter({
+        url,
+        method: 'GET',
+      }, (error, res) => {
+        t.equal(error, null, 'no error');
+        t.isLike(res.url, url, 'contains expected path');
+      });
+  
+      t.end();
+    });
+
+    t.test('should block when manifest appears after fragment identifier', (t) => {
+      filter({
+        url: '/repos/angular/angular/contents/test-main.js#/package.json',
+        method: 'GET',
+      }, (error, res) => {
+        t.equal(error.message, 'blocked', 'has been blocked');
+        t.equal(res, undefined, 'no follow allowed');
+      });
+  
+      t.end();
+    });
+
+    t.end();
+  });
+
+  t.end();
+});
+
 test('filter on body', t => {
   const filter = Filters(require(__dirname + '/../fixtures/relay.json'));
 
@@ -214,10 +254,10 @@ test('filter on body', t => {
 
 });
 
-test('filter on querystring', t => {
+test('Filter on querystring', t => {
   const filter = Filters(require(__dirname + '/../fixtures/relay.json'));
 
-  t.plan(9);
+  t.plan(10);
   t.pass('filters loaded');
 
   filter({
@@ -253,12 +293,43 @@ test('filter on querystring', t => {
     t.equal(error.message, 'blocked', 'has been blocked');
     t.equal(res, undefined, 'no follow allowed');
   });
+
+  t.test('fragment identifiers validation', (t) => {
+    t.plan(2);
+
+    t.test('should not allow access to sensitive files by putting the manifest after a fragment', (t) => {
+      filter({
+        url: '/filtered-on-query?filePath=/path/to/sensitive/file#package.json',
+        method: 'GET',
+      }, (error, res) => {
+        t.equal(error.message, 'blocked', 'errors as expected');
+        t.equal(res, undefined, 'follow not allowed');
+      });
+
+      t.end();
+    });
+
+    t.test('should ignore any non-manifest files after the fragment identifier', (t) => {
+      filter({
+        url: '/filtered-on-query?filePath=/path/to/package.json#/some-other-file',
+        method: 'GET',
+      }, (error, res) => {
+        t.equal(error, null, 'no error');
+        t.equal(res.url, '/filtered-on-query?filePath=/path/to/package.json',
+          'contains the expected manifest in the query string');
+      });
+
+      t.end();
+    });
+      
+    t.end();
+  });
 });
 
-test('filter on query and body', t => {
+test('Filter on query and body', t => {
   const filter = Filters(require(__dirname + '/../fixtures/relay.json'));
 
-  t.plan(9);
+  t.plan(10);
   t.pass('filters loaded');
 
   filter({
@@ -312,6 +383,43 @@ test('filter on query and body', t => {
   }, (error, res) => {
     t.equal(error.message, 'blocked', 'has been blocked');
     t.equal(res, undefined, 'no follow allowed');
+  });
+
+  t.test('fragment identifiers validation', (t) => {
+    t.plan(2);
+
+    t.test('should not allow access to sensitive files by putting the manifest after a fragment', (t) => {
+      filter({
+        url: '/filtered-on-query-and-body?filePath=/path/to/sensitive/file.js#package.json',
+        method: 'POST',
+        body: jsonBuffer({
+          commits: []
+        })
+      }, (error, res) => {
+        t.equal(error.message, 'blocked', 'errors as expected');
+        t.equal(res, undefined, 'follow not allowed');
+      });
+
+      t.end();
+    });
+
+    t.test('should ignore any non-manifest files after the fragment identifier', (t) => {    
+      filter({
+        url: '/filtered-on-query-and-body?filePath=/path/to/package.json#/sensitive/file.js',
+        method: 'POST',
+        body: jsonBuffer({
+          commits: []
+        })
+      }, (error, res) => {
+        t.equal(error, null, 'no error');
+        t.equal(res.url, '/filtered-on-query-and-body?filePath=/path/to/package.json',
+          'contains the expected manifest in the query string');
+      });
+
+      t.end();
+    });
+
+    t.end();
   });
 });
 
