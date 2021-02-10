f3b3f3785569a932a9f475c6f31a54449415450e
mlgsfe76 <mlgsfe76@gmail.com>
2021-02-10 02:38:40 -0600

    Delete lintVerifier.sh

--- lintVerifier.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-set -eo pipefail
-
-echo "Linting JSON files..."
-for file in $(find ./client-templates -type f -name *.json.sample); do
-    echo "${file}"
-    node -r "fs" -e "JSON.parse(fs.readFileSync('${file}'))"
-done
