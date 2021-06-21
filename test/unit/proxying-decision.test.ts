ebede586425b1572503d63bec9bb2b7aa094673c
Carlos <carlos@snyk.io>
2021-06-21 15:29:11 +0100

    test: split no_proxy test cases

--- test/unit/proxying-decision.test.ts
+++ test/unit/proxying-decision.test.ts
@@ -16,9 +16,9 @@ describe('Proxy decision', () => {
     expect(shouldProxy(parse('https://broker.snyk.io'))).toEqual(false);
   });
 
-  it('should not proxy URLs set via `no_proxy` env var', () => {
+  it('should not proxy URL set via `no_proxy` env var', () => {
     // eslint-disable-next-line @typescript-eslint/camelcase
-    const oldestDomainOnINET = (process.env.no_proxy = 'symbolics.com');
+    process.env.no_proxy = 'symbolics.com';
     // eslint-disable-next-line @typescript-eslint/camelcase
     process.env.https_proxy = 'https://localhost:8888';
 
@@ -26,21 +26,48 @@ describe('Proxy decision', () => {
     const {
       shouldProxy,
     } = require('../../lib/patch-https-request-for-proxying');
-    const url = parse('http://' + oldestDomainOnINET);
+    const url = parse('http://symbolics.com');
     expect(shouldProxy(url)).toEqual(false);
     expect(shouldProxy(parse('https://shambhala.org/'))).toEqual(true);
   });
 
-  it('should not proxy URLs set via `NO_PROXY` env var', () => {
-    const dontProx = 'wiki.c2.com';
-    process.env.NO_PROXY = 'symbolics.com,' + dontProx;
+  it('should not proxy URL set via `NO_PROXY` env var', () => {
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    process.env.NO_PROXY = 'symbolics.com';
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    process.env.https_proxy = 'https://localhost:8888';
+
+    // loaded now, for config to be reloaded after env vars
+    const {
+      shouldProxy,
+    } = require('../../lib/patch-https-request-for-proxying');
+    const url = parse('http://symbolics.com');
+    expect(shouldProxy(url)).toEqual(false);
+    expect(shouldProxy(parse('https://shambhala.org/'))).toEqual(true);
+  });
+
+  it('should not proxy URL set via `NO_PROXY` env var when containing query params', () => {
+    process.env.NO_PROXY = 'symbolics.com';
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    process.env.https_proxy = 'https://localhost:8888';
+
+    // loaded now, for config to be reloaded after env vars
+    const {
+      shouldProxy,
+    } = require('../../lib/patch-https-request-for-proxying');
+
+    expect(shouldProxy(parse('http://symbolics.com/?hello'))).toEqual(false);
+  });
+  it('should not proxy list of URLs set via `NO_PROXY` env var', () => {
+    process.env.NO_PROXY = 'symbolics.com,wiki.c2.com';
     process.env.HTTPS_PROXY = 'https://localhost:8888';
 
     const {
       shouldProxy,
     } = require('../../lib/patch-https-request-for-proxying');
 
-    expect(shouldProxy(parse(`https://${dontProx}/?XeroxParc`))).toEqual(false);
-    expect(shouldProxy(parse('http://silibank.net.kp'))).toEqual(true);
+    expect(shouldProxy(parse(`https://wiki.c2.com`))).toEqual(false);
+    expect(shouldProxy(parse(`https://symbolics.com`))).toEqual(false);
+    expect(shouldProxy(parse('https://silibank.net.kp'))).toEqual(true);
   });
 });
