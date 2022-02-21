32828d08655ea5248bb74429df4e227c6f86ba4a
yeforriak <yeforriak@gmail.com>
2022-02-21 23:00:41 +0000

    feat: do not expand variables that contain passwords, because passwords can contain $ symbol

--- test/unit/config.test.ts
+++ test/unit/config.test.ts
@@ -1,13 +1,29 @@
 describe('config', () => {
-  it('contain application config', () => {
+  it('parses application config', () => {
     const foo = (process.env.FOO = 'bar');
     const token = (process.env.BROKER_TOKEN = '1234');
-    process.env.FOO_BAR = '$FOO/bar';
+    const passwordWithSpecialChars = (process.env.BITBUCKET_PASSWORD =
+      "!\"#%&'()*+,-./:;$<=>?@[\\]^_`{|}~'");
+    const brokerClientValidationAuthHeader =
+      (process.env.BROKER_CLIENT_VALIDATION_AUTHORIZATION_HEADER =
+        "username:!\"#%&'()*+,-./:;$<=>?@[\\]^_`{|}~'");
+    const brokerClientValidationBasicAuth =
+      (process.env.BROKER_CLIENT_VALIDATION_BASIC_AUTH =
+        "username:!\"#%&'()*+,-./:;$<=>?@[\\]^_`{|}~'");
+
+    process.env.EXPANDABLE_ENV_VAR = '$FOO/bar';
 
     const config = require('../../lib/config');
 
     expect(config.foo).toEqual(foo);
     expect(config.brokerToken).toEqual(token);
-    expect(config.fooBar).toEqual('bar/bar');
+    expect(config.expandableEnvVar).toEqual('bar/bar');
+    expect(config.bitbucketPassword).toEqual(passwordWithSpecialChars);
+    expect(config.brokerClientValidationAuthorizationHeader).toEqual(
+      brokerClientValidationAuthHeader,
+    );
+    expect(config.brokerClientValidationBasicAuth).toEqual(
+      brokerClientValidationBasicAuth,
+    );
   });
 });
