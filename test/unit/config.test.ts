dde40fd4a32a5ff461e97b8966edc53564776e56
Oliver Benning <oliver.benning@snyk.io>
2022-05-25 10:41:19 -0400

    Add tests for complex config

--- test/unit/config.test.ts
+++ test/unit/config.test.ts
@@ -3,11 +3,13 @@ describe('config', () => {
     const foo = (process.env.FOO = 'bar');
     const token = (process.env.BROKER_TOKEN = '1234');
     process.env.FOO_BAR = '$FOO/bar';
+    const complex_token = (process.env.COMPLEX_TOKEN='1234$$%#@!$!$@$$$')
 
     const config = require('../../lib/config');
 
     expect(config.foo).toEqual(foo);
     expect(config.brokerToken).toEqual(token);
     expect(config.fooBar).toEqual('bar/bar');
+    expect(config.complex_token).toEqual(complex_token);
   });
 });
