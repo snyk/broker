41ac0db851afda4108406f861193fd8f0cab1804
Antoine Arlaud <antoine@snyk.io>
2022-11-03 15:06:10 +0100

    chore: add iac and code accept rules injection via env var
    chore: add iac extensions yml,tpl
    
    chore: update error message
    
    fix: adjust endpoint type and fix azure repos cases
    
    fix: adjust endpoint auth for gitlab code

--- test/unit/runtime-rules-hotloading.test.ts
+++ test/unit/runtime-rules-hotloading.test.ts
@@ -1,22 +1,96 @@
-import { readFileSync } from 'fs';
 import * as path from 'path';
 import * as loadFilterRules from '../../lib/filter-rules-loading';
 
-function loadFixture(name: string) {
-  const fixturePath = path.join(__dirname, '..', 'fixtures', name);
-  const fixture = readFileSync(fixturePath, { encoding: 'utf-8' });
-
-  return fixture;
-}
+const scmRulesToTest = [
+  'azure-repos',
+  'bitbucket-server',
+  'github-com',
+  'github-enterprise',
+  'gitlab',
+];
 
 describe('filter Rules Loading', () => {
-  it('Loads normal accept file', () => {
-    const rules = JSON.parse(loadFixture(path.join('accept', 'ghe.json')));
-    const loadedRules = loadFilterRules(
-      'ghe.json',
-      path.join(__dirname, '..', 'fixtures/accept'),
-    );
+  test.each(scmRulesToTest)(
+    'Loads normal accept file - Testing %s',
+    (folder) => {
+      const loadedRules = loadFilterRules(
+        'accept.json.sample',
+        path.join(__dirname, '../..', `client-templates/${folder}`),
+      );
 
-    expect(loadedRules).toEqual(rules);
-  });
+      expect(loadedRules).toMatchSnapshot();
+    },
+  );
+
+  test.each(scmRulesToTest)(
+    'Skip injection if no or invalid IAC extensions - Testing %s',
+    (folder) => {
+      process.env.ACCEPT_IAC = 'rf';
+      const loadedRules = loadFilterRules(
+        'accept.json.sample',
+        path.join(__dirname, '../..', `client-templates/${folder}`),
+      );
+
+      expect(loadedRules).toMatchSnapshot();
+      delete process.env.ACCEPT_IAC;
+    },
+  );
+
+  test.each(scmRulesToTest)(
+    'Injection of valid IAC extensions - Testing %s',
+    (folder) => {
+      process.env.ACCEPT_IAC = 'tf,yaml, json,yml,tpl';
+      const loadedRules = loadFilterRules(
+        'accept.json.sample',
+        path.join(__dirname, '../..', `client-templates/${folder}`),
+      );
+
+      expect(loadedRules).toMatchSnapshot();
+      delete process.env.ACCEPT_IAC;
+    },
+  );
+
+  test.each(scmRulesToTest)(
+    'Injection of valid IAC extensions - Testing %s',
+    (folder) => {
+      process.env.ACCEPT_IAC = 'tf,json';
+      const loadedRules = loadFilterRules(
+        'accept.json.sample',
+        path.join(__dirname, '../..', `client-templates/${folder}`),
+      );
+
+      expect(loadedRules).toMatchSnapshot();
+      delete process.env.ACCEPT_IAC;
+    },
+  );
+
+  test.each(scmRulesToTest)(
+    'Injection of valid CODE rules - Testing %s',
+    (folder) => {
+      process.env.ACCEPT_CODE = 'true';
+      const loadedRules = loadFilterRules(
+        'accept.json.sample',
+        path.join(__dirname, '../..', `client-templates/${folder}`),
+      );
+
+      expect(loadedRules).toMatchSnapshot();
+      delete process.env.ACCEPT_CODE;
+    },
+  );
+
+  test.each(scmRulesToTest)(
+    'Injection of valid CODE rules and IAC extensions (yaml only) - Testing %s',
+    (folder) => {
+      process.env.ACCEPT_CODE = 'true';
+      process.env.ACCEPT_IAC = 'yaml';
+      const loadedRules = loadFilterRules(
+        'accept.json.sample',
+        path.join(__dirname, '../..', `client-templates/${folder}`),
+      );
+
+      expect(loadedRules).toMatchSnapshot();
+      delete process.env.ACCEPT_CODE;
+      delete process.env.ACCEPT_IAC;
+    },
+  );
 });
