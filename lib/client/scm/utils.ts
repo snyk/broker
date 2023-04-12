4d22bdf98b96dcae60516f7c896a6ffe6fc8bfa1
Antoine Arlaud <antoine@snyk.io>
2023-04-12 23:54:03 +0200

    chore: adding missing dep and formatting

--- lib/client/scm/utils.ts
+++ lib/client/scm/utils.ts
@@ -1,48 +1,45 @@
-
 import * as openpgp from 'openpgp';
 
-
 export const createCommitSignature = async (
-    commit: {
-        message: string;
-        tree: string;
-        parents: string[];
-        author: { name: string; email: string; date: Date };
-        committer: { name: string; email: string; date: Date };
-        signature?: string;
-    },
-    privateKeyArmored: string,
-    passphrase: string,
+  commit: {
+    message: string;
+    tree: string;
+    parents: string[];
+    author: { name: string; email: string; date: Date };
+    committer: { name: string; email: string; date: Date };
+    signature?: string;
+  },
+  privateKeyArmored: string,
+  passphrase: string,
 ): Promise<string | undefined> => {
-    const privateKey = await openpgp.readPrivateKey({
-        armoredKey: privateKeyArmored,
-    });
+  const privateKey = await openpgp.readPrivateKey({
+    armoredKey: privateKeyArmored,
+  });
 
-    const decryptedPrivateKey = await openpgp.decryptKey({
-        privateKey,
-        passphrase,
-    });
+  const decryptedPrivateKey = await openpgp.decryptKey({
+    privateKey,
+    passphrase,
+  });
 
-    const message = stringifyCommit(commit);
+  const message = stringifyCommit(commit);
 
-    const detachedSignature = await openpgp.sign({
-        message: await openpgp.createMessage({ text: message }),
-        signingKeys: [decryptedPrivateKey],
-        detached: true,
-    });
+  const detachedSignature = await openpgp.sign({
+    message: await openpgp.createMessage({ text: message }),
+    signingKeys: [decryptedPrivateKey],
+    detached: true,
+  });
 
-    return detachedSignature.toString().replace(/\r\n/g, '\n').trim();
-}
-
+  return detachedSignature.toString().replace(/\r\n/g, '\n').trim();
+};
 
 export function stringifyCommit({
-    message,
-    tree,
-    parents,
-    author,
-    committer = author,
+  message,
+  tree,
+  parents,
+  author,
+  committer = author,
 }) {
-    return `tree ${tree}
+  return `tree ${tree}
   parent ${parents[0]}
   author ${userToString(author)}
   committer ${userToString(committer)}
@@ -50,21 +47,21 @@ export function stringifyCommit({
 }
 
 function userToString(user: { date: string; name: string; email: string }) {
-    const date = new Date(user.date);
-    const timestamp = Math.floor(date.getTime() / 1000);
-    const timezone = normalizeOffset(date.getTimezoneOffset());
+  const date = new Date(user.date);
+  const timestamp = Math.floor(date.getTime() / 1000);
+  const timezone = normalizeOffset(date.getTimezoneOffset());
 
-    return `${user.name} <${user.email}> ${timestamp} ${timezone}`;
+  return `${user.name} <${user.email}> ${timestamp} ${timezone}`;
 }
 
 function normalizeOffset(offset: number) {
-    const prefix = offset <= 0 ? '+' : '-';
-    const hours = Math.abs(offset / 60)
-        .toString(10)
-        .padStart(2, '0');
-    const minutes = Math.abs(offset % 60)
-        .toString(10)
-        .padStart(2, '0');
+  const prefix = offset <= 0 ? '+' : '-';
+  const hours = Math.abs(offset / 60)
+    .toString(10)
+    .padStart(2, '0');
+  const minutes = Math.abs(offset % 60)
+    .toString(10)
+    .padStart(2, '0');
 
-    return `${prefix}${hours}${minutes}`;
+  return `${prefix}${hours}${minutes}`;
 }
