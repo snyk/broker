c3e80567cfc0e74d471940dd48afe028be6c1163
Pavel Sorokin <60606414+pavel-snyk@users.noreply.github.com>
2023-04-18 16:42:19 +0200

    test: revert changes in client-ca-test.ts

--- test/functional/client-ca.test.ts
+++ test/functional/client-ca.test.ts
@@ -13,104 +13,8 @@
 //   '../fixtures/certs/server/fullchain.pem',
 // );
 
-import * as fs from 'fs';
-// import * as https from 'https';
-import * as path from 'path';
-// import axios from 'axios';
-import { BrokerClient, createBrokerClient } from '../setup/broker-client';
-import { BrokerServer, createBrokerServer } from '../setup/broker-server';
-import { createTestLogger } from '../helpers/logger';
-import { TestWebServer, createTestWebServer } from '../setup/test-web-server';
-import { requestAsync } from '../utils';
-
-const LOG = createTestLogger();
-const fixtures = path.resolve(__dirname, '..', 'fixtures');
-const serverAccept = path.join(fixtures, 'server', 'filters.json');
-const clientAccept = path.join(fixtures, 'client', 'filters-https.json');
-const sslCertificateKey = path.join(fixtures, 'certs/server/privkey.pem');
-const sslCertificate = path.resolve(fixtures, 'certs/server/fullchain.pem');
-
 describe('correctly use supplied CA cert on client for connections', () => {
-  let tws: TestWebServer;
-  let bs: BrokerServer;
-  let bc: BrokerClient;
-  let brokerToken: string;
-
-  beforeAll(async () => {
-    process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
-    // check if all fixtures files exist
-    try {
-      fs.readFileSync(sslCertificate);
-      fs.readFileSync(sslCertificateKey);
-      fs.readFileSync(serverAccept);
-      fs.readFileSync(clientAccept);
-    } catch (error) {
-      LOG.error(error);
-      throw new Error('check paths to fixtures');
-    }
-
-    tws = await createTestWebServer({
-      sslCertificatePath: sslCertificate,
-      sslCertificateKeyPath: sslCertificateKey,
-    });
-
-    bs = await createBrokerServer({ filters: serverAccept });
-
-    bc = await createBrokerClient({
-      brokerServerUrl: `http://localhost:${bs.port}`,
-      brokerToken: '12345',
-      filters: clientAccept,
-    });
-
-    await new Promise((resolve) => {
-      bs.server.io.on('connection', (socket) => {
-        socket.on('identify', (clientData) => {
-          brokerToken = clientData.token;
-          resolve(brokerToken);
-        });
-      });
-    });
-  });
-  afterAll(async () => {
-    await tws.server.close();
-    setTimeout(async () => {
-      await bc.client.close();
-    }, 100);
-    await new Promise<void>((resolve) => {
-      bc.client.io.on('close', () => {
-        resolve();
-      });
-    });
-
-    setTimeout(async () => {
-      await bs.server.close();
-    }, 100);
-    await new Promise<void>((resolve) => {
-      bs.server.io.on('close', () => {
-        resolve();
-      });
-    });
-  });
-
-  it.skip('get an error trying to connect to a server with unknown CA', async () => {
-    // const { status } = await axios.post(
-    //   `http://localhost:${bs.port}/broker/${brokerToken}/echo-body`,
-    //   {},
-    //   {
-    //     //httpsAgent: new https.Agent({ rejectUnauthorized: true }),
-    //     validateStatus: () => false,
-    //   },
-    // );
-    try {
-      const url = `http://localhost:${bs.port}/broker/${brokerToken}/echo-body`;
-      const response = await requestAsync({ url, method: 'post', json: true });
-      LOG.info(response, 'response from asyncResponse');
-    } catch (error) {
-      LOG.info(error);
-    }
-    expect(2).toEqual(2);
-    //expect(res.statusCode).toEqual(500);
-  });
+  it.skip('get an error trying to connect to a server with unknown CA', async () => {});
 });
 
 // t.test(
@@ -124,29 +28,29 @@ describe('correctly use supplied CA cert on client for connections', () => {
 //      * 5. start broker in client mode with supplied CA cert
 //      * 6. send request to the server and expect success
 //      */
-
+//
 //     const { echoServerPort, testServer } = createTestServer();
 //
 //     t.teardown(async () => {
 //       testServer.close();
 //     });
-
+//
 //     process.env.ACCEPT = 'filters.json';
+//
 //     process.chdir(path.resolve(root, '../fixtures/server'));
 //     process.env.BROKER_TYPE = 'server';
 //     let clientPort;
 //     const serverPort = port();
 //     const server = await app.main({ port: serverPort });
-
+//
 //     process.chdir(path.resolve(root, '../fixtures/client'));
 //     process.env.BROKER_TYPE = 'client';
 //     process.env.BROKER_TOKEN = '12345';
 //     process.env.BROKER_SERVER_URL = `http://localhost:${serverPort}`;
 //     process.env.ORIGIN_PORT = echoServerPort;
-//     process.env.ACCEPT = 'filters-https.json';
-//      We need to connect to the https version of _internal service_
+//     process.env.ACCEPT = 'filters-https.json'; // We need to connect to the https version of _internal service_
 //     let client = await app.main({ port: port() });
-
+//
 //     // wait for the client to successfully connect to the server and identify itself
 //     const promise = new Promise((resolve) => {
 //       server.io.once('connection', async (socket) => {
@@ -165,12 +69,12 @@ describe('correctly use supplied CA cert on client for connections', () => {
 //               t.equal(res.statusCode, 500, '500 statusCode');
 //             },
 //           );
-
+//
 //           await t.test('close', async (t) => {
 //             client.close();
 //             t.ok('client closed');
 //           });
-
+//
 //           // const p = new Promise((resolve) => {
 //           // await t.test('launch a new client with CA set', async (t) => {
 //           //     server.io.on('connection', async (socket) => {
@@ -184,7 +88,7 @@ describe('correctly use supplied CA cert on client for connections', () => {
 //           //     client = await app.main({ port: clientPort });
 //           //   })
 //           // });
-
+//
 //           // await t.test('successfully broker POST with CA set', async (t) => {
 //           //   const url = `http://localhost:${serverPort}/broker/${token}/echo-body`;
 //           //   let { res } = await requestAsync({
@@ -194,7 +98,7 @@ describe('correctly use supplied CA cert on client for connections', () => {
 //           //   });
 //           //   t.equal(res.statusCode, 200, '200 statusCode');
 //           // });
-
+//           //
 //           // await t.test(
 //           //   'successfully call systemcheck with CA set',
 //           //   async (t) => {
@@ -203,7 +107,7 @@ describe('correctly use supplied CA cert on client for connections', () => {
 //           //     t.equal(res.statusCode, 200, '200 statusCode');
 //           //   },
 //           // );
-
+//
 //           t.test('clean up', async (t) => {
 //             //await client.close();
 //             setTimeout(() => {
