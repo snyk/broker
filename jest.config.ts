2db03e7b79ddc8bb30baa7c875b23005f034fd75
Pavel Sorokin <60606414+pavel-snyk@users.noreply.github.com>
2024-02-02 12:43:48 +0100

    feat: upgrade to Node.js 20
    - use heredoc syntax for multiline run instructions
    - test: upgrade jest to 29

--- jest.config.ts
+++ jest.config.ts
@@ -1,20 +1,25 @@
 import type { Config } from 'jest';
 
-const config: Config = {
-  bail: 1,
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/test/tsconfig.json',
+export default async (): Promise<Config> => {
+  process.env.LOG_LEVEL = process.env.TEST_LOG_LEVEL || 'fatal';
+
+  return {
+    bail: 1,
+    maxConcurrency: 1,
+    maxWorkers: 1,
+    reporters: ['default', ['jest-junit', { outputDirectory: 'reports' }]],
+    rootDir: 'test',
+    // keep old format: https://jestjs.io/blog/2022/08/25/jest-29
+    snapshotFormat: {
+      escapeString: true,
+      printBasicPrototype: true,
     },
-  },
-  maxConcurrency: 1,
-  maxWorkers: 1,
-  modulePathIgnorePatterns: ['<rootDir>/dist'],
-  preset: 'ts-jest',
-  reporters: ['default', ['jest-junit', { outputDirectory: 'reports' }]],
-  testMatch: ['<rootDir>/test/**/*.test.ts'],
-  testEnvironment: 'node',
-  testTimeout: 20_000,
-  verbose: true,
+    testRegex: '.*\\.test\\.ts$',
+    testEnvironment: 'node',
+    testTimeout: 20_000,
+    transform: {
+      '^.+\\.(t|j)s$': 'ts-jest',
+    },
+    verbose: true,
+  };
 };
-export default config;
