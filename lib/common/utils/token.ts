e52fe5eec5dfdd905d2c0eb0bd868a4e07d1c579
Antoine Arlaud <antoine@snyk.io>
2024-07-30 17:02:28 -0400

    chore: add ghsa plugin error handling

--- lib/common/utils/token.ts
+++ lib/common/utils/token.ts
@@ -14,6 +14,14 @@ export function maskToken(token) {
   return token.slice(0, 4) + '-...-' + token.slice(-4);
 }
 
+export function maskSCMToken(token) {
+  if (!token || token === '') {
+    return '';
+  }
+
+  return token.slice(0, 3) + '****' + token.slice(-3);
+}
+
 export const extractBrokerTokenFromUrl = (urlString) => {
   const regex = /^\/broker\/([a-z0-9-]+)\//;
   return urlString.match(regex) ? urlString.match(regex)[1] : null;
