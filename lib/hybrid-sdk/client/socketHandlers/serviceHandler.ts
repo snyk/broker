ce1d80aff1b08ef00a76c14c35f45edb175ac06b
Antoine Arlaud <antoine@snyk.io>
2025-03-14 13:05:20 +0100

    feat: add svc channel to notify cfg+filter changes to client

---
+++ lib/hybrid-sdk/client/socketHandlers/serviceHandler.ts
@@ -0,0 +1,66 @@
+import { log as logger } from '../../../logs/logger';
+import { getClientOpts } from '../config/configHelpers';
+import { syncClientConfig } from '../connectionsManager/synchronizer';
+import {
+  getGlobalIdentifyingMetadata,
+  getWebsocketConnections,
+} from '../connectionsManager/manager';
+
+export const serviceHandler = async (msg, cb) => {
+  const clientOptions = getClientOpts();
+  const command = msg.url;
+  const requestId = msg.headers['snyk-request-id'];
+  logger.debug({ command, requestId }, 'Service message received.');
+  if (!clientOptions) {
+    cb({
+      status: 501,
+      headers: { requestId },
+      body: JSON.stringify({
+        msg: 'Service channel is not available in classic Broker.',
+      }),
+    });
+  } else {
+    switch (command) {
+      case '/filters/reload':
+        logger.debug({}, 'Reloading filters.');
+        cb({
+          status: 200,
+          headers: { requestId },
+          body: JSON.stringify({ msg: `Filters reloaded.` }),
+        });
+        break;
+      case '/config/reload':
+        logger.debug({}, 'Reloading config.');
+        if (clientOptions.config.universalBrokerEnabled) {
+          cb({
+            status: 200,
+            headers: { requestId },
+            body: JSON.stringify({ msg: `Synchronizing Config.` }),
+          });
+          await syncClientConfig(
+            clientOptions,
+            getWebsocketConnections(),
+            getGlobalIdentifyingMetadata(),
+          );
+        } else {
+          logger.debug({}, 'Service command not active in classic broker.');
+          cb({
+            status: 501,
+            headers: { requestId },
+            body: 'Service command not active in classic broker.',
+          });
+        }
+        break;
+      default:
+        logger.error(
+          { command, requestId },
+          'Unknown service message received.',
+        );
+        cb({
+          status: 400,
+          headers: { requestId },
+          body: 'Unknown service command.',
+        });
+    }
+  }
+};
